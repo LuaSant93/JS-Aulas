7be53dfc890fa5520a3ecdeeaaf661d9fa5d85d1
LuaSant93 <luan10091993@hotmail.com>
2023-11-07 15:34:17 -0300

    Aulas Jscript

--- Exercicios/exercicio06.js
+++ Exercicios/exercicio06.js
@@ -1,4 +1,4 @@
-
+/*
 
 const numero = 7;
 
@@ -7,4 +7,18 @@ for (let i = 1; i <= 10; i++) {
     
 }
 
-/*Vai Corinthians*/
+Vai Corinthians*/
+
+
+
+const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+for (let i = 0; i < numeros.length; i++) {
+    const numero = numeros[i];
+    
+    if (numero % 2 === 0) {
+        console.log(i);
+    console.log(numero);
+    }
+}
